eeb9710f10af1f545db37546abb6bb8561c5ec2f
Chasman <chasman182@gmail.com>
2021-03-06 08:36:21 -0500

    Initial commit

---
+++ src/main/java/com/chasbott/winenotebook/WinenotebookApplication.java
@@ -0,0 +1,13 @@
+package com.chasbott.winenotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinenotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinenotebookApplication.class, args);
+	}
+
+}
